13198521831c2d600f9ba224842de515c5fce197
lslemon <l.slemon1@nuigalway.ie>
2020-01-15 21:17:30 +0000

    Next commit

---
+++ app/src/main/java/com/example/hiking_application/ui/main/CartoMapsActivity.java
@@ -0,0 +1,17 @@
+package com.example.hiking_application.ui.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.hiking_application.R;
+
+public class CartoMapsActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carto_maps);
+
+
+    }
+}
